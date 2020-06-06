d0b00b61a9a11e8ee139ef77a267f62a6757ba04
PortosaurusBot <claperone.portasaurus@gmail.com>
2020-06-06 11:17:53 +0100

    Auto generated portfolio

---
+++ precache-manifest.19cf7b6f55cb3789721b4944f3ba7177.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95efe47bdee7e1d2fe74ceda5d76e631",
+    "url": "portosaurusdev.github.io/ethansk-313d4/index.html"
+  },
+  {
+    "revision": "9bbc26767616a69162c3",
+    "url": "portosaurusdev.github.io/ethansk-313d4/static/css/2.2c0f828a.chunk.css"
+  },
+  {
+    "revision": "97c6e62ed6265447fbcd",
+    "url": "portosaurusdev.github.io/ethansk-313d4/static/css/main.4319e8d8.chunk.css"
+  },
+  {
+    "revision": "9bbc26767616a69162c3",
+    "url": "portosaurusdev.github.io/ethansk-313d4/static/js/2.5b76354d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "portosaurusdev.github.io/ethansk-313d4/static/js/2.5b76354d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97c6e62ed6265447fbcd",
+    "url": "portosaurusdev.github.io/ethansk-313d4/static/js/main.38a8be78.chunk.js"
+  },
+  {
+    "revision": "b1c4742e787c09f600c8",
+    "url": "portosaurusdev.github.io/ethansk-313d4/static/js/runtime-main.90465f89.js"
+  },
+  {
+    "revision": "d1055b19fe53d68547369e312be91bac",
+    "url": "portosaurusdev.github.io/ethansk-313d4/static/media/defaultbanner.d1055b19.png"
+  }
+]);
